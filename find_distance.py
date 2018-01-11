c68ee20e8fd768022bb664c7f00723f182830fcf
aben20807 <aben20807@gmail.com>
2018-01-11 22:11:42 +0800

    Clean code

--- find_distance.py
+++ find_distance.py
@@ -18,8 +18,8 @@ for i in range(12, 41):
     row_s = ""
     for j in range(41):
         print(i, j, loc[i], loc[j])
-        orig_coord = loc[i] # '22.9872,120.2114'
-        dest_coord = loc[j] # '22.98624,120.21703'
+        orig_coord = loc[i]
+        dest_coord = loc[j]
         url = "http://maps.googleapis.com/maps/api/distancematrix/json?origins={0}&destinations={1}&mode=driving&&sensor=false".format(str(orig_coord),str(dest_coord))
         result = json.load(urllib.request.urlopen(url))
         # print(json.dumps(result, indent=4, sort_keys=True))
@@ -30,7 +30,6 @@ for i in range(12, 41):
     f.write(row_s + '\n')
     time.sleep(5)
     dis_list.append(new)
-# print(dis)
-# print(dis_list)
+
 print('\n'.join([''.join(['{:5}'.format(item) for item in row])
       for row in dis_list]))
