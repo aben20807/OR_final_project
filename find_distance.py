fa205948b4ef89155dc343189d8e33c10473e471
aben20807 <aben20807@gmail.com>
2018-01-11 22:10:36 +0800

    Can create data set

--- find_distance.py
+++ find_distance.py
@@ -1,6 +1,7 @@
 import urllib.request
 import json
 import csv
+import time
 
 loc = []
 with open('location.csv') as csvfile:
@@ -9,10 +10,27 @@ with open('location.csv') as csvfile:
         loc.append(row['location'])
 
 print(loc)
-orig_coord = '22.9872,120.2114'
-dest_coord = '22.98624,120.21703'
-url = "http://maps.googleapis.com/maps/api/distancematrix/json?origins={0}&destinations={1}&mode=driving&&sensor=false".format(str(orig_coord),str(dest_coord))
-result = json.load(urllib.request.urlopen(url))
-dis = result['rows'][0]['elements'][0]['distance']['value']
-# print(json.dumps(result, indent=4, sort_keys=True))
-print(dis)
+
+dis_list = []
+f = open('distance.csv','w')
+for i in range(12, 41):
+    new = []
+    row_s = ""
+    for j in range(41):
+        print(i, j, loc[i], loc[j])
+        orig_coord = loc[i] # '22.9872,120.2114'
+        dest_coord = loc[j] # '22.98624,120.21703'
+        url = "http://maps.googleapis.com/maps/api/distancematrix/json?origins={0}&destinations={1}&mode=driving&&sensor=false".format(str(orig_coord),str(dest_coord))
+        result = json.load(urllib.request.urlopen(url))
+        # print(json.dumps(result, indent=4, sort_keys=True))
+        dis = result['rows'][0]['elements'][0]['distance']['value']
+        print(dis)
+        row_s += str(dis) + ", "
+        new.append(dis)
+    f.write(row_s + '\n')
+    time.sleep(5)
+    dis_list.append(new)
+# print(dis)
+# print(dis_list)
+print('\n'.join([''.join(['{:5}'.format(item) for item in row])
+      for row in dis_list]))
